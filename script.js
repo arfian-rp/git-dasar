4a6359a61048b7f55acd12091c3d0380c566954b
arfian-rp <arfianpradana07@gmail.com>
2021-12-14 18:53:31 +0700

    fix alert in script.js

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 document.querySelector(".credit").addEventListener("click", () => {
   alert("created by eko");
+  alert("created by eka");
+  alert("created by adi");
   alert("created by duwi");
 });
